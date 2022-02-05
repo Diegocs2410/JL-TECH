86ce3eda61db6028a1d975ff21f4d0aabefcf488
Diego Armando Carreño Serrano <diegocs2410@gmail.com>
2022-02-05 09:41:55 -0500

    Navbar

--- jl-tech-test/src/main.jsx
+++ jl-tech-test/src/main.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { AuthUserProvider } from './context/AuthUser'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:4000/api/v1'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthUserProvider>
+      <App />
+    </AuthUserProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
